59630d9a98c6894d1c79d407e2d65f4aae860d44
Nayana Agrawal <101566994@georgebrown.ca>
2025-06-10 03:06:52 +0000

    create blackjack page

---
+++ app/blackjack/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Page() {
+    return <>
+        <div id="container-fluid">
+            <div className="row">
+                <div className="col-10 offset-1">
+
+                </div>
+            </div>
+        </div>
+    </>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -12,6 +12,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="description" content="Who killed William?" />
+        <meta name="author" content="Nayana Agrawal & William Desbiens" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
+
+        <link href="./global.css" rel="sylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
