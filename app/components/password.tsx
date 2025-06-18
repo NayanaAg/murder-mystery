89d484940f34720749448c1a5509433206bc9c8e
Nayana Agrawal <101566994@georgebrown.ca>
2025-06-18 02:46:10 +0000

    start pw component

---
+++ app/components/password.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Data from "../puzzles.json";
+
+export default function PasswordPlease(props) {
+
+    const puzzle = Data[props.puzzle];
+
+            function passwordChecker() {
+                
+            }
+
+
+    return <>
+        <h1>PLEASE ENTER THE PASSWORD</h1>
+
+            
+    </>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -21,11 +21,11 @@ export default function RootLayout({
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossOrigin="anonymous" />
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossOrigin="anonymous"></script>
 
-        <link href="./global.css" rel="sylesheet" />
-
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
         <link href="https://fonts.googleapis.com/css2?family=Faculty+Glyphic&family=Lancelot&display=swap" rel="stylesheet" />
+
+        <link href="./global.css" rel="sylesheet" />
       </head>
       <body>{children}</body>
     </html>
