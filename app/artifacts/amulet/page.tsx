f2978bc6da51ebd31bf4e50395d8edc1eacd9afa
Nayana Agrawal <101566994@georgebrown.ca>
2025-06-18 12:39:41 -0400

    Update page.tsx

--- app/artifacts/amulet/page.tsx
+++ app/artifacts/amulet/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
                     <h1>The Amulet</h1>
                     <p>The Dancing Man's amulet may be the most critical component of the three-piece artifact. Although it may look unassuming on the surface, the necklace acts as an anchor for the spirit's soul and energy.</p>
 
-                    <p>When paired with the amulet, <strong>the Dancing Man loses his ability to disappear and reappear at will.</strong> This simple yet effective techique has been used by many historians and occultists to safely transport the artifact to museum exhibits. Great peril lies ahead for those who attempt to separate and resell the amulet separately. It's a tempting offer, considering the rare gemstones embedded in its avian eyes, but your life is too high a price to pay for it.</p>
+                    <p>When paired with the amulet, <strong>the Dancing Man loses his ability to disappear and reappear at will.</strong> This simple yet effective technique has been used by many historians and occultists to safely transport the artifact to museum exhibits. Great peril lies ahead for those who attempt to separate and resell the amulet separately. It's a tempting offer, considering the rare gemstones embedded in its avian eyes, but your life is too high a price to pay for it.</p>
                 </div>
             </div>
         </div>
