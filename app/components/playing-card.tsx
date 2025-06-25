c17e8bd153efd91bc160d1ee2669d676fc31c265
Nayana Agrawal <101566994@georgebrown.ca>
2025-06-25 03:07:46 +0000

    trying to fix window undefined error

--- app/components/playing-card.tsx
+++ app/components/playing-card.tsx
@@ -2,12 +2,12 @@
 
 import React from "react";
 import "../blackjack/blackjack.css";
-import * as playingCards from "../blackjack.json";
+import playingCards from "../blackjack.json";
 import FlipPage from "react-flip-page";
 
 export default function PlayingCard(props) {
 
-    let card = playingCards[props.card]
+    const card = playingCards[props.card]
 
     return <>
         <div className="card-container" key={card.id}>
