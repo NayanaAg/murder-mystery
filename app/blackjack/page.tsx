79524e7277ff116285072a308b0f81afcbc63f8e
Nayana Agrawal <101566994@georgebrown.ca>
2025-06-25 22:19:55 +0000

    layout of blackjack page

--- app/blackjack/page.tsx
+++ app/blackjack/page.tsx
@@ -7,13 +7,43 @@ import PlayingCard from "../components/playing-card";
 export default function Page() {
     return <>
         <div id="container-fluid">
-            <div className="row">
-                <div className="col-10 offset-1">
-                    <h1>Welcome to Casino Royale</h1>
-                    
-                    <PlayingCard card="1" />
+
+            <div className="row g-0 mt-5 p-4">
+                <div className="text-center d-flex">
+                    <div className="col">
+                        <PlayingCard card="0" />
+                    </div>
+                    <div className="col">
+                        <PlayingCard card="2" />
+                    </div>
+                    <div className="col">
+                        <PlayingCard card="4" />
+                    </div>
                 </div>
             </div>
+
+            <section className="mb-3" id="player-cards">
+                <div className="row g-0 mt-5 p-4">
+                    <div className="text-center d-flex">
+                        <div className="col">
+                            <PlayingCard card="0" />
+                        </div>
+                        <div className="col">
+                            <PlayingCard card="2" />
+                        </div>
+                        <div className="col">
+                            <PlayingCard card="4" />
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row d-flex">
+                    <div className="col-10 offset-1 text-center">
+                        <button type="button" className="btn btn-danger">Hit</button>
+                        <button type="button" className="btn btn-info">Stand</button>
+                    </div>
+                </div>
+            </section>
         </div>
     </>
 }
